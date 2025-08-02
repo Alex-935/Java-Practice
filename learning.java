0ae07e8e2b76405284ed2c214f8dc5d9dd002d81
alex-935 <alex.potter3@hotmail.co.uk>
2025-08-02 09:48:57 +0100

    printf statements

--- learning.java
+++ learning.java
@@ -5,7 +5,7 @@ public class learning {
 
     public static void main(String[] args) {
         
-        mathClass();
+        printf();
     }
 
     public static void arithmetic() {
@@ -104,5 +104,81 @@ public class learning {
 
         System.out.print(eight);
 
+    }
+
+    public static void printf() {
+
+        /* printf is used to format an output
+         %[flags][width][.precision][specifier-character]
+
+         Specifier Characters:
+
+         string - s
+         char - c
+         int - d
+         double - f (for float)
+         boolean - b
+        */
+
+        String name = "Spongebob";
+        char firstLetter = 'S';
+        int age = 30;
+        double height = 60.5;
+        boolean isEmployed = true;
+
+        System.out.printf("Hello %s\n", name);
+        System.out.printf("Your name starts with a %c\n", firstLetter);
+        System.out.printf("You are %d years old\n", age);
+        System.out.printf("You are %f inches tall\n", height);
+        System.out.printf("Employed: %b\n", isEmployed);
+
+        System.out.printf("%s is %d years old\n", name, age);
+
+
+        //Flags
+
+        // + = output a plus before the number
+        // , = grouping sperator
+        // ( = negative numbers will be enclosed in ()
+        // space = display a minus if negative, space if positive
+
+        double price1 = 9.99;
+        double price2 = 100.15;
+        double price3 = -54.01;
+        double price4 = 1345789.543;
+
+        System.out.printf("%.2f\n", price1);
+        System.out.printf("%+.2f\n", price2);
+        System.out.printf("%.2f\n", price3);
+        //order doesnt matter
+        System.out.printf("%,+.2f\n", price4);
+        System.out.printf("%+,.2f\n", price4);
+
+
+        //width
+
+        // 0number (e.g 04) = 0 padding (to width of 4)
+        // number = right justified padding
+        // negative number = left justified pading
+
+        int id1 = 1;
+        int id2 = 23;
+        int id3 = 456;
+        int id4 = 7890;
+
+        System.out.printf("%04d\n", id1);
+        System.out.printf("%04d\n", id2);
+        System.out.printf("%04d\n", id3);
+        System.out.printf("%04d\n", id4);
+
+        System.out.printf("%4d\n", id1);
+        System.out.printf("%4d\n", id2);
+        System.out.printf("%4d\n", id3);
+        System.out.printf("%4d\n", id4);
+
+        System.out.printf("%-4d\n", id1);
+        System.out.printf("%-4d\n", id2);
+        System.out.printf("%-4d\n", id3);
+        System.out.printf("%-4d\n", id4);
     }
 }
