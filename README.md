3b78d0009e57377c876871152766078f075a5c40
Alex-935 <alex.potter3@hotmail.co.uk>
2025-08-07 12:00:47 +0100

    Number guessing game

--- README.md
+++ README.md
@@ -6,6 +6,7 @@
 - interestCalculator.java: Calculates the amount of interest after a given number of years for a given interest rate.
 - learning.java: Various random pieces of Java covering a range of topics
 - madLibs.java: Creates a program that plays the game Mad Libs.
+- numberGuessingGame.java: Keep guessing the number until you get it. Higher and lower responses will help you narrow down the possible values.
 - shoppingCart: Calculates the total price of an item given the price and quantity.
 - temperatureConverter.java: Converts from celcius to fahrenheit and vice versa for a given temperature.
 - validateUsername.java: Validates and enetered uisername to make sure it meets the requirements.
--- numberGuessingGame.java
+++ numberGuessingGame.java
@@ -1,3 +1,37 @@
+import java.util.Random;
+import java.util.Scanner;
+
 public class numberGuessingGame {
     
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        Random random = new Random();
+
+        int randNum = random.nextInt(1, 101);
+        int guess;
+        int attempts = 0;
+
+        System.out.println("Welcome to the number guessing game");
+
+        do {
+
+            System.out.print("Please guess a number between 1 and 100: ");
+            guess = scanner.nextInt();
+
+            if (guess < randNum) {
+                System.out.println("Higher!");
+            } else if (guess > randNum) {
+                System.out.println("Lower!");
+            }
+
+            attempts ++;
+            
+        } while (guess != randNum);
+
+        System.out.println("\nCongratulations!, you guessed the number.\n" +
+            "You took " + attempts + " guesses.\n");
+        
+        scanner.close();
+    }
 }
