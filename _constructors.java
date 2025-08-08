b04302eed328e2ecc09bc11b815b5ee5275e8a65
Alex-935 <alex.potter3@hotmail.co.uk>
2025-08-08 09:44:31 +0100

    Overloaded Constructors

--- _constructors.java
+++ _constructors.java
@@ -12,6 +12,15 @@ public class _constructors {
         Student student2 = new Student("Patrick", 32, 1.5);
 
         student1.study();
+
+
+        /*Overloaded constructors = Allows a class  to have multiple constructors
+        with different parameter lists
+        Enable objects to be initialised in various ways.
+        */
+
+        User user1 = new User();
+        User user2 = new User("Anakin", "deathStickEnjoyer@podracing.com", 19);
     }
 
     public static class Student {
@@ -21,6 +30,7 @@ public class _constructors {
         double gpa;
         boolean isEnrolled;
 
+        //constructor uses same name as class
         Student(String name, int age, double gpa) {
 
             this.name = name;
@@ -31,6 +41,42 @@ public class _constructors {
 
         void study() {
             System.out.println(this.name + " is studying.");
+        }
+    }
+
+
+    public static class User {
+
+        String username;
+        String email;
+        int age;
+
+        User() {
+
+            this.username = "Guest";
+            this.email = "Not Provided";
+            this.age = 0;
+        }
+
+        User(String username) {
+
+            this.username = username;
+            this.email = "Not Provided";
+            this.age = 0;
+        }
+
+        User(String username, String email) {
+
+            this.username = username;
+            this.email = email;
+            this.age = 0;
+        }
+
+        User(String username, String email, int age) {
+
+            this.username = username;
+            this.email = email;
+            this.age = age;
         }
     }
 
