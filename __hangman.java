985e6dd48ac79cf3169bd530205d20e4f80f98fc
Alex-935 <alex.potter3@hotmail.co.uk>
2025-08-09 07:57:47 +0100

    Hangman outline

--- __hangman.java
+++ __hangman.java
@@ -2,5 +2,21 @@ public class __hangman {
     
     public static void main(String[] args) {
 
+        /* 
+        //Pick a random word from list of strings
+        //display hidden version of word
+        //ask user for input
+        //check input against word
+            - fill in any correct letters
+            - add to gallows if no matches
+        //Draw in gallows
+
+        // if incorrect guesses = num
+            - agame over
+            - reveal word
+        
+            // play again?
+        */
+
     }
 }
